8999cb148108c95da4e7e954f3f2d8ebf1b12f35
wscats <2587575267@qq.com>
2018-01-12 11:17:59 +0800

    day5

--- day5/webpack-vue/index.js
+++ day5/webpack-vue/index.js
@@ -1,30 +1,57 @@
 import Vue from "vue";
+// 路由
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
 
 import "weui";
 // 全局样式
 import "./template/base.css";
 
-// 引入公共组件
-import xheader from "./components/xheader.vue";
-import xsearch from "./components/xsearch.vue";
-import xpanel from "./components/xpanel.vue";
-import xfooter from "./components/xfooter.vue";
 
+// 选项卡组件
+import wechat from "./components/tabs/wechat.vue";
+import discovery from "./components/tabs/discovery.vue";
+import contact from "./components/tabs/contact.vue";
+import mine from "./components/tabs/mine.vue";
+
+// 容器组件
+import tab from "./components/container/tab.vue";
+import chat from "./components/container/chat.vue";
+
+// 配置路由
+var router = new VueRouter({
+    routes: [{
+        path: "/tab",
+        component: tab,
+        children: [{
+                path: 'wechat',
+                component: wechat
+            },
+            {
+                path: 'discovery',
+                component: discovery
+            },
+            {
+                path: 'contact',
+                component: contact
+            },
+            {
+                path: 'mine',
+                component: mine
+            }
+        ]
+    }, {
+        path: "/chat",
+        component: chat
+    }]
+})
 
 new Vue({
-    el:"#app",
-    template:`
+    el: "#app",
+    router,
+    template: `
         <div>
-            <xheader />
-            <xsearch />
-            <xpanel />
-            <xfooter />
+            <router-view></router-view> 
         </div>
-    `,
-    components:{
-        xheader,
-        xsearch,
-        xpanel,
-        xfooter
-    }
+    `
 })
